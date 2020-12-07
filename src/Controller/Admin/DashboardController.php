9e03f2832e6affbf6ab19e5c831219ae887ea57b
Riwanlm <riwanlm@gmail.com>
2020-12-07 18:28:46 +0100

    Mapping de l'entité Production --> 'make:admin:crud' >> Illustration ne génère pas d'erreur mais ne fonctionne pas <<

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -31,5 +32,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', User::class);
         yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Produits', 'fa fa-tag', Product::class);
     }
 }
