19ee19b34481bae9c69d90a1252ad60a5444d75b
Riwanlm <riwanlm@gmail.com>
2021-01-26 17:26:28 +0100

    Création de la vue récapitulatif panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,21 +2,38 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use App\Classe\Cart;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
+        $cartComplete = [];
 
-        return $this->render('cart/index.html.twig');
+        foreach($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
+        ]);
     }
 
     /**
